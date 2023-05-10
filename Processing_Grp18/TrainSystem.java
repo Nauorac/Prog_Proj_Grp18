b81a9b3b830b5a9cb9c8a8a822118a0e2e51b3e2
Sébastien Baudoux <sebastien.baudoux@student.unamur.be>
2023-05-10 12:06:40 +0200

    Ajout event apparition train

--- Processing_Grp18/TrainSystem.java
+++ Processing_Grp18/TrainSystem.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
 import java.util.Map;
 
+// **-SEB-*******************************************************
+// LE CODE ICI REPREND LES VARIABLES ET INVARIANTS DE LA MACHINE
+
 public class TrainSystem {
     private Map<String, Integer> Gares = new HashMap<>();
     private Map<String, Integer> Gares_urgence = new HashMap<>();
@@ -204,31 +207,12 @@ for (String ville : nb_train.keySet()) {
     Feux.put("Huy_Lge", false);
 
 //  theorem @inv24 Station_to_Line_Feux ∈ {Namur↦Bruxelles, Namur↦Liege, Bruxelles↦Namur, Bruxelles↦Liege, Liege↦Bruxelles, Liege↦Namur} → dom(Feux)
-    
-
 
 //  @inv25 Secondary_Station_to_Line_Feux ∈ { Namur↦Bruxelles,Bruxelles↦Namur, Namur↦Liege, Liege↦Namur, Liege↦Bruxelles, Bruxelles↦Liege} → dom(Feux)
 
-
-
 // @inv26 Line_to_Station_Feux ∈ {Namur↦Bruxelles, Namur↦Liege, Bruxelles↦Namur, Bruxelles↦Liege, Liege↦Bruxelles, Liege↦Namur} → dom(Feux)
 
-
-
-
-
-
-
-
-
 // @inv27 Line_to_Secondary_Station_Feux ∈ {Namur↦Bruxelles, Namur↦Liege, Bruxelles↦Namur, Bruxelles↦Liege, Liege↦Bruxelles, Liege↦Namur} → dom(Feux)
-
-
-
-
-
-
-
 
 // @inv28 Appli_Voyageur ∈ {Namur,Liege,Bruxelles,Huy,Leuven,Ottignies} → BOOL    
     Map<String, Boolean> Appli_Voyageur = new HashMap<>();
@@ -238,5 +222,88 @@ for (String ville : nb_train.keySet()) {
     Appli_Voyageur.put("Huy", false);
     Appli_Voyageur.put("Leuven", false);
     Appli_Voyageur.put("Ottignies", false);
+
+
+
+
+    // *-SEB-***********************************************************************************************
+    // LE CODE CI DESSOUS REPREND DEPUIS LE CONTEXT, ET LE DEBUT DE LA MACHINE, TOUT CE QUI CONCERNE L EVENEMENT
+    // APPARITION TRAIN
+    // ********************************************************************
+
+    public class TrainSystem {
+        public enum Gare {
+            Namur, Bruxelles, Liege, Huy, Ottignies, Leuven
+        }
+    
+        public enum Quai {
+            Quai1, Quai2
+        }
+    
+        private Map<Gare, Integer> gares;
+        private Map<Gare, Integer> garesUrgence;
+        private Map<Gare, Integer> nbTrain;
+        private int nbTrainTotal;
+        private Map<Gare, Boolean> freinageLine;
+        private Map<Gare, Map<Quai, Boolean>> quai;
+        private boolean stateSystem;
+        private boolean stateBrake;
+        private Map<Gare, Integer> garePrincipale;
+    
+        public TrainSystem() {
+            initialisation();
+        }
+    
+        private void initialisation() {
+            gares = new HashMap<>();
+            gares.put(Gare.Namur, 1);
+            gares.put(Gare.Bruxelles, 2);
+            gares.put(Gare.Liege, 3);
+    
+            nbTrain = new HashMap<>();
+            for (Gare g : Gare.values()) {
+                nbTrain.put(g, 0);
+            }
+    
+            nbTrainTotal = 0;
+            freinageLine = new HashMap<>();
+            for (Gare g : new Gare[]{Gare.Bruxelles, Gare.Namur, Gare.Liege}) {
+                freinageLine.put(g, false);
+            }
+    
+            stateSystem = false;
+            stateBrake = false;
+    
+            garePrincipale = new HashMap<>();
+            garePrincipale.put(Gare.Bruxelles, 0);
+            garePrincipale.put(Gare.Namur, 0);
+            garePrincipale.put(Gare.Liege, 0);
+    
+            quai = new HashMap<>();
+            for (Gare g : Gare.values()) {
+                Map<Quai, Boolean> quaiGare = new HashMap<>();
+                quaiGare.put(Quai.Quai1, false);
+                if (g == Gare.Namur || g == Gare.Bruxelles || g == Gare.Liege) {
+                    quaiGare.put(Quai.Quai2, false);
+                }
+                quai.put(g, quaiGare);
+            }
+        }
+
+    // Evenement APPARITION TRAIN
+    public void apparitionTrain(Gare selectGare) {
+        if (nbTrain.get(selectGare) < 2 && nbTrainTotal < 4 && !freinageLine.get(selectGare) && !stateSystem && !stateBrake
+                && garePrincipale.containsKey(selectGare) && !quai.get(selectGare).get(Quai.Quai1)) {
+            nbTrain.put(selectGare, nbTrain.get(selectGare) + 1);
+            nbTrainTotal++;
+            quai.get(selectGare).put(Quai.Quai1, true);
+        }
+    }
+    // Autres méthodes de gestion des événements et de mise à jour des variables peuvent être ajoutées ici
+        
+    public static void main(String[] args) {
+        TrainSystem trainSystem
+
+
 
 
